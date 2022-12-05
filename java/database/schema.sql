2326382d102db930bb442ab5b511c19fd947fa7d
Logan Hammock <Logant.hammock@gmail.com>
2022-12-05 15:15:50 -0500

    testing

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -11,3 +11,30 @@ CREATE TABLE users (
 );
 
 COMMIT TRANSACTION;
+
+
+BEGIN TRANSACTION;
+
+DROP TABLE IF EXISTS collection;
+
+CREATE TABLE collection (
+collection id SERIAL,
+collection_name varchar (50) NOT NULL UNIQUE,
+username varchar (50) NOT NULL,
+comic_name (varchar) (50) NOT NULL
+);
+
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
+
+DROP TABLE IF EXISTS comic;
+
+CREATE TABLE comic(
+comic id SERIAL,
+comic_name varchar (50) NOT NULL UNIQUE,
+author varchar (50) NOT NULL,
+release_date date NOT NULL
+);
+
+COMMIT TRANSACTION;
