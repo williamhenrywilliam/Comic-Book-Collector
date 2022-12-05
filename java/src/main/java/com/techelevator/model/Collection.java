c4e7ef179a88bc22f9500520c452abc938099843
Dalton Lambert <lambertd757@gmail.com>
2022-12-05 15:41:21 -0500

    Added Usercontroller, comic and collection model classes

---
+++ java/src/main/java/com/techelevator/model/Collection.java
@@ -0,0 +1,10 @@
+package com.techelevator.model;
+
+public class Collection {
+
+    private int collectionId;
+    private String collectionName;
+    private String userName;
+    private String comic_name;
+
+}
