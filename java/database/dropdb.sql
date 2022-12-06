70db16c108e9bbcc8d09741747e2c70cd386e312
William Yetka <whyetka@gmail.com>
2022-12-06 12:29:16 -0500

    we changed name of dataabse to ComicBookCollection from final_capstone in the database sql files

--- java/database/dropdb.sql
+++ java/database/dropdb.sql
@@ -5,9 +5,10 @@
 -- The following line terminates any active connections to the database so that it can be destroyed
 SELECT pg_terminate_backend(pid)
 FROM pg_stat_activity
-WHERE datname = 'final_capstone';
+--this was previouisly 'final capstone'
+WHERE datname = 'ComicBookCollection';
 
-DROP DATABASE final_capstone;
+DROP DATABASE ComicBookCollection;
 
 DROP USER final_capstone_owner;
 DROP USER final_capstone_appuser;
