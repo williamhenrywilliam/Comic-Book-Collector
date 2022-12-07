ff11b126ac5b7ae0f37c071844490b8539f6ec8e
William Yetka <whyetka@gmail.com>
2022-12-06 19:28:56 -0500

    front end changes, grid on the App page

---
+++ vue/src/components/FooterBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="footer" class="class">
+        <h2>THIS IS THE FOOTER</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'footer-box'
+}
+</script>
+
+<style scoped>
+#footer {
+    background-color: blueviolet;
+}
+</style>
