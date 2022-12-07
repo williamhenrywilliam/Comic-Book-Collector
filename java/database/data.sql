1fd2002991cdf0a7b435d48a19cdc529d274b5a1
Logan Hammock <Logant.hammock@gmail.com>
2022-12-07 11:00:26 -0500

    stash

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,11 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO collection (collectionname,username,comicname) VALUES ('daltonscomics','dalton','spidernman');
+INSERT INTO collection (collectionname,username,comicname) VALUES ('daltonscomics','dalton','superman');
+INSERT INTO comic (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+
 COMMIT TRANSACTION;
+
+
+
