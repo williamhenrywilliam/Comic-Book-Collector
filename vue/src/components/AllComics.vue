fffc8b28f06dd2784317dba92a137e000cfc3e67
William Yetka <whyetka@gmail.com>
2022-12-12 17:53:42 -0500

    created a button to add comic to a collection and update the comic to the database

--- vue/src/components/AllComics.vue
+++ vue/src/components/AllComics.vue
@@ -6,18 +6,28 @@
     </div>
     
     <div id="comics-container">
-      <span v-for="comic in searchedComics" v-bind:key="comic.comic_id" class="comic" @click="comic.flipped = !comic.flipped">
-        <span v-if="comic.flipped">
-          <span>{{comic.comicName}}</span>
-          <span>{{comic.author}}</span>
-          <span>{{comic.releaseDate}}</span>
+        <span v-for="comic in searchedComics" v-bind:key="comic.comic_id" class="comic" @click="comic.flipped = !comic.flipped">
+         
+          <span v-if="comic.flipped" id="comic-details">
+            <span>{{comic.comicName}}</span>
+            <span>{{comic.author}}</span>
+            <span>{{comic.releaseDate}}</span>
+          </span>
+
+          <div v-else>
+            <img :src="comic.imageURL" alt="Spider-Man comic">
+          </div>
+
+          <form v-on:submit.prevent="addComicToACollection">
+            <button>Add To Collection</button>
+            <label for="user-collections">Select Collection to Add to</label>
+            <select name="user-collections" id="user-collections">
+              <option v-for="collection in userCollections" v-bind:key="collection.collectionName" value="COMEBACKTOTHIS">{{collection.collectionName}}</option>
+            </select>
+          </form>
+
         </span>
-        <div v-else>
-          <img :src="comic.imageURL" alt="Spider-Man comic">
-        </div>
-      </span>
     </div>
-  <!-- Hello Dalton -->
   </div>
 </template>
 
@@ -28,6 +38,7 @@ export default {
   data(){
     return {
       comics: [],
+      collections: this.$store.state.collections,
       searchText: ""
     }
   },
@@ -47,15 +58,34 @@ export default {
       return this.comics.filter((comic)=> {
         return comic.comicName.toLowerCase().includes(this.searchText.toLowerCase()) || comic.author.toLowerCase().includes(this.searchText.toLowerCase());
       })
+    },
+    userCollections(){
+      return this.$store.state.collections;
     }
   },
+  mounted(){
+      this.$store.dispatch("getAllCollections");
+  },
+  methods: {
+    addComicToACollection(){
+      const newComic = {
+        comicName: "Test Name2",
+        author: "Test Author2",
+        releaseDate: "2000-01-02",
+        collectionId: 5,
+        imageURL: "Test URL String2"
+      }
+
+      ComicService.addComicToACollection(newComic);
+    }
+  }
 }
 </script>
 <style scoped>
 span.comic {
   border: 3px solid black;
-  height: 150px;
-  width: 100px;
+  height: 400px;
+  width: 264px;
   margin: 10px 10px 10px 10px;
   padding: 10px 10px 10px 10px;
   display:flex;
@@ -68,7 +98,7 @@ span.comic {
 img {
   width: 100%;
   height: 100%;
-  object-fit:cover;
+  object-fit: cover;
 }
 
 #search-bar {
@@ -80,17 +110,12 @@ img {
   display: flex;
   flex-wrap: wrap;
 }
-span.comic-details {
- display:flex;
-    flex-direction: column;
-    text-align: center;
-    justify-content: space-between;
-    color: #fff;
-}
 
 span.comic {
   /* existing styles */
+  box-shadow: 0 0 5px rgba(0,0,0,0.5);
   transition: all 0.3s ease;
+  margin-bottom: 50px;
 }
 
 span.comic:hover {
@@ -98,8 +123,12 @@ span.comic:hover {
   box-shadow: 0 0 10px rgba(0,0,0,0.5);
 }
 
-span.comic {
-  /* existing styles */
-  box-shadow: 0 0 5px rgba(0,0,0,0.5);
+#comic-details {
+  display: flex;
+  flex-direction: column;
+}
+
+span > form {
+  margin-top: 20px;
 }
 </style>
