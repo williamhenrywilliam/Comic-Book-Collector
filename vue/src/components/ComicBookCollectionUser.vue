655e12ec29b1eeed1de8e79beda62a1d73be38de
William Yetka <whyetka@gmail.com>
2022-12-13 23:04:56 -0500

    changed the profile page so that it uses a dropdown to display one collection of comics at a atime

--- vue/src/components/ComicBookCollectionUser.vue
+++ vue/src/components/ComicBookCollectionUser.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    <div class="comic-book-collection-user">
-      <h2>This is a User Collection</h2>
-
-      <div v-for="collection in userCollectionsOnly" v-bind:key="collection.name"> {{collection.collectionName}}
-        <div class="comic-book-container-user">
-          <comic-book-card v-for="comic in featuredCollection" v-bind:comic="comic" v-bind:key="comic.author"/>
-        </div>
-      </div>
+    <div class="all-user-collections">
+      <h2>This is all of the signed in User's collections</h2>
       
+      
+      <label for="user-collections-dropdown">Select the Collection You Would Like to View: </label>
+      <select name="user-collections-dropdown" id="dropdown-choice" class="user-collections-dropdown" v-model="selectedCollectionId">
+        <option value="" disabled selected>---</option>
+        <option v-for="collection in userCollections" v-bind:key="collection.collectionName" :value="collection.collectionId">{{collection.collectionName}}</option>
+      </select>
+      
+
+      <div class="selected-collection-comics">
+        <comic-book-card v-for="comic in comicsInSelectedCollection" v-bind:comic="comic" v-bind:key="comic.author"/>
+      </div>
+
     </div>
   </div>
 </template>
@@ -17,60 +23,40 @@
 import ComicBookCard from "./ComicBookCard.vue"
 
 export default {
-    name: 'comic-book-collection-user',
-    components: {
-      ComicBookCard
-    },
-    data(){
-      return{
-        comicsDB: [],
-        allComics: [],
-        allCollections: [],
-        userCollectionsOnly: [],
-        userCollectionsIdsOnly: []
-      }
-    },
-    created() {
-      this.allComics = this.$store.state.comicsDB;
-      this.allCollections = this.$store.state.collections;
-      this.userCollectionsOnly = this.allCollections.filter((collection) => {return collection.collectionId > 9});
-      this.userCollections
-    },
-    mounted(){
-      this.$store.dispatch("getAllComics");
-      this.$store.dispatch("getAllCollections");
-    },
-    computed: {
-      featuredCollection(){
-        return this.$store.state.comicsDB.filter((comic) => {
-          return comic.collectionId === 3;
-        }); 
-      },
-      userCollections(){
-        return this.$store.state.collections.filter((collection) => {
-          //THE 9 here is because we have 9 starter collections. so a user collection is guaranteed to be greater than 9. if we add more starter collections we need to update this number
-          return collection.collectionId > 9;
-        })
-      },
-      methods: {
-        testMethod(){
-          return this.$store.state.comicsDB.filter((comic) => {
-          return comic.collectionId === 3;
-        }); 
-        }
-      }
+  name: 'comic-book-collection-user',
+  components: {
+    ComicBookCard
+  },
+  data() {
+    return {
+      comics: [],
+      collections: this.$store.state.collections,
+      selectedCollectionId: ""
     }
-
+  },
+  computed: {
+    userCollections(){
+      return this.$store.state.collections.filter((collection) => {
+        return collection.collectionId > 9;
+      });
+    },
+    comicsInSelectedCollection(){
+      return this.$store.state.comicsDB.filter((comic) => {
+        return comic.collectionId === this.selectedCollectionId;
+      })
+    }
+  }
 }
 </script>
 
 <style>
-div.comic-book-collection-user{
-  border: 5px solid greenyellow
+div.all-user-collections{
+  border: 5px solid pink;
 }
 
-div.comic-book-container-user{
+div.selected-collection-comics {
+  border:5px solid brown;
   display: flex;
-    justify-content: space-evenly;
+  flex-wrap: wrap;
 }
 </style>
