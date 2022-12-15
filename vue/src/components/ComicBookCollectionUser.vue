24883c2354de969b98142b802cacf136ec6863e4
William Yetka <whyetka@gmail.com>
2022-12-14 21:38:59 -0500

    added more statistics

--- vue/src/components/ComicBookCollectionUser.vue
+++ vue/src/components/ComicBookCollectionUser.vue
@@ -1,16 +1,14 @@
 <template>
   <div>
     <div class="all-user-collections">
-      <h2>This is all of the signed in User's collections</h2>
-      
-      
+            
       <label for="user-collections-dropdown">Select the Collection You Would Like to View: </label>
       <select name="user-collections-dropdown" id="dropdown-choice" class="user-collections-dropdown" v-model="selectedCollectionId">
         <option value="" disabled selected>---</option>
         <option v-for="collection in userCollections" v-bind:key="collection.collectionName" :value="collection.collectionId">{{collection.collectionName}}</option>
       </select>
       
-
+    
       <div class="selected-collection-comics">
         <comic-book-card v-for="comic in comicsInSelectedCollection" v-bind:comic="comic" v-bind:key="comic.author"/>
       </div>
@@ -21,6 +19,7 @@
 
 <script>
 import ComicBookCard from "./ComicBookCard.vue"
+//import ComicService from "../services/ComicService"
 
 export default {
   name: 'comic-book-collection-user',
@@ -31,7 +30,8 @@ export default {
     return {
       comics: [],
       collections: this.$store.state.collections,
-      selectedCollectionId: ""
+      selectedCollectionId: "",
+      selectedCollectionComicCount: ""
     }
   },
   computed: {
@@ -45,7 +45,17 @@ export default {
         return comic.collectionId === this.selectedCollectionId;
       })
     }
+  },
+  /*
+  methods: {
+    getComicCountByCollectionId(id){
+      ComicService.getComicCountByCollectionId(id).then((response) => {
+        const comicCount = response.data
+        return comicCount
+      })
+    }
   }
+  */
 }
 </script>
 
