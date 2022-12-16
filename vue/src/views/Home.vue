58500c40d9db1a800651e125d1b96a9e6f50795e
Dalton Lambert <lambertd757@gmail.com>
2022-12-15 19:06:25 -0500

    added font changes and picture to homepage

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -8,15 +8,16 @@
 
     <div class="statistics">
       <ul>
-        <li>Our Website has {{comicCount}} Comics! THIS NEEDS TO BE FIXED</li>
-        <li>Our Website has {{usersCount}} current Users! Get in on the ground floor while you still can!</li>
-        <li>Our Website has {{collectionCount}} different collections! Such Variety!</li>
-        <li>Our Website has comics from {{authorCount}} different authors!</li>
+        <li>Our Website has {{comicCount}} Comics!  <img src="https://static.thenounproject.com/png/7433-200.png" alt="comic book icon" class="comic-book-icon" /></li>
+        <li>Our Website has {{usersCount}} current Users! Get in on the ground floor while you still can! <img src="https://cdn-icons-png.flaticon.com/512/33/33308.png" alt="comic book icon" class="comic-book-icon" /></li>
+        <li>Our Website has {{collectionCount}} different collections! Such Variety! <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Bimetrical_icon_folder_black.svg/1024px-Bimetrical_icon_folder_black.svg.png" alt="comic book icon" class="comic-book-icon" /></li>
+        <li>Our Website has comics from {{authorCount}} different authors! <img src="https://camo.githubusercontent.com/a50d5bd3ee846fc94720056bf6db040681809512df69e8764fe5406f5ed8b462/68747470733a2f2f662e636c6f75642e6769746875622e636f6d2f6173736574732f363132303433312f313639333233352f62373337366163382d356538622d313165332d393135382d6661313961313263663133612e706e67" alt="comic book icon" class="comic-book-icon" /></li>
       </ul>
     </div>
     
     <div class="picture">
-      <img src="../assets/marvel1.jpeg" alt="Marvel Comic Heroes" />
+      <h2>Latest Issue</h2>
+      <img src="https://i.annihil.us/u/prod/marvel/i/mg/3/b0/61d4c518767b4/clean.jpg" alt="Marvel Comic Heroes" />
     </div>
 
     <div class="featured-collection">
@@ -82,6 +83,8 @@ div.home {
     "statistics picture"
     "featured-collection featured-collection"
     ;
+    
+
 }
 
 div.title {
@@ -124,6 +127,40 @@ div.picture img {
   margin-right: auto;
   width: 75%;
 }
+
+div.statistics {
+  grid-area: statistics;
+  border: 2px solid white;
+  margin: 10px 10px 10px 10px;
+  font-family: monospace, serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  text-shadow: 1px 1px 1px rgba(0,0,0,0.5);
+  
+}
+
+.comic-book-icon {
+  height: 50px;
+  width: 50px;
+   vertical-align: middle;
+}
+
+
+li {
+margin: 10px 0;
+padding: 10px 0;
+list-style-type: none;
+}
+
+h2 {
+text-align: center;
+font-family: monospace, serif;
+text-shadow: 1px 1px 1px rgba(0,0,0,0.5);
+}
+
 
 </style>
 
